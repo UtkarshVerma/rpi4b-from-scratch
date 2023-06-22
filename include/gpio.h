34008cf4a5c8fdbd933785d55c4acc32bf2b91cb
Utkarsh Verma <utkarshverma@protonmail.com>
2023-06-22 17:34:39 +0530

    Fix GPIO function definitions

--- include/gpio.h
+++ include/gpio.h
@@ -3,12 +3,12 @@
 typedef enum {
     GPIO_INPUT,
     GPIO_OUTPUT,
+    GPIO_AF5,
+    GPIO_AF4,
     GPIO_AF0,
     GPIO_AF1,
     GPIO_AF2,
     GPIO_AF3,
-    GPIO_AF4,
-    GPIO_AF5,
 } gpio_function;
 
 int gpio_set_function(unsigned int pin, gpio_function value);
