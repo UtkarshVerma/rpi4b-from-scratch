9a550f342bc0e57e0a58e4d81ec5eb57b2736672
Utkarsh Verma <utkarshverma@protonmail.com>
2023-06-22 15:31:39 +0530

    Improve how registers are accessed

--- src/gpio.c
+++ src/gpio.c
@@ -4,45 +4,41 @@
 
 #define GPIO_MAX_PIN 53
 
-typedef enum {
-    GPFSEL0                = MMIO_REG_ADDR(GPIO, 0x00),
-    GPSET0                 = MMIO_REG_ADDR(GPIO, 0x1c),
-    GPCLR0                 = MMIO_REG_ADDR(GPIO, 0x28),
-    GPIO_PUP_PDN_CTRL_REG0 = MMIO_REG_ADDR(GPIO, 0xe4),
-} gpio_reg;
+#define GPFSEL0                MMIO_REG(GPIO, 0x00)
+#define GPSET0                 MMIO_REG(GPIO, 0x1c)
+#define GPCLR0                 MMIO_REG(GPIO, 0x28)
+#define GPIO_PUP_PDN_CTRL_REG0 MMIO_REG(GPIO, 0xe4)
 
-typedef unsigned int gpio_reg_value;
-
-static int gpio_set_reg(gpio_reg base_reg, unsigned int pin,
-                        gpio_reg_value value, unsigned int field_size) {
+static int gpio_set_reg(mem_ptr base_reg, unsigned int pin, mem_data value,
+                        unsigned int field_size) {
     if (pin > GPIO_MAX_PIN) return 1;
 
     unsigned int field_mask = (1 << field_size) - 1;
     if (value > field_mask) return 1;
 
     // GPIO registers are uniformly subdivided
-    unsigned int n_fields = sizeof(gpio_reg_value) * 8 / field_size;
+    unsigned int n_fields = sizeof(mem_data) * 8 / field_size;
 
     // Registers are sequentially mapped for each `n_field` GPIOs
-    gpio_reg reg       = base_reg + (pin / n_fields) * sizeof(gpio_reg_value);
+    mem_ptr reg        = base_reg + pin / n_fields;
     unsigned int shift = (pin % n_fields) * field_size;
 
-    unsigned int tmp = MEM_REG_VALUE(reg);
+    unsigned int tmp = *reg;
     tmp &= ~(field_mask << shift);  // Clear the field
     tmp |= value << shift;          // Set value to the field
-    MEM_REG_VALUE(reg) = tmp;
+    *reg = tmp;
 
     return 0;
 }
 
 int gpio_set_function(unsigned int pin, gpio_function value) {
-    return gpio_set_reg(GPFSEL0, pin, value, 3);
+    return gpio_set_reg(&GPFSEL0, pin, value, 3);
 }
 
 int gpio_clear_pin(unsigned int pin) {
-    return gpio_set_reg(GPCLR0, pin, 1, 1);
+    return gpio_set_reg(&GPCLR0, pin, 1, 1);
 }
 
 int gpio_set_pin(unsigned int pin) {
-    return gpio_set_reg(GPSET0, pin, 1, 1);
+    return gpio_set_reg(&GPSET0, pin, 1, 1);
 }
